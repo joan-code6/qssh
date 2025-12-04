71aff8c165af7c61b87298b783f55350e3d036c7
Joan Code <bennetjoanwegener@gmail.com>
2025-12-04 14:46:53 +0100

    updating structure

--- qssh/src/qssh/__init__.py
+++
@@ -1,9 +0,0 @@
-"""qssh - Quick SSH session manager."""
-
-__version__ = "0.2.1"
-__author__ = "benne"
-
-from .session import SessionManager
-from .connector import SSHConnector
-
-__all__ = ["SessionManager", "SSHConnector", "__version__"]
